90ddd13076709d2f76a76f7b9c0566c8f61e99e0
Student <Student@A125531.ad.csueastbay.edu>
2019-03-01 15:27:10 -0800

    commit clone

---
+++ Conflict_Clone/src/conflict_clone/Conflict_Clone.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conflict_clone;
+
+/**
+ *
+ * @author Student
+ */
+public class Conflict_Clone {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int hello = 5;
+        // TODO code application logic here
+    }
+    
+}
